3ded878d903068fd9dd69ce90f756ec5e1067156
prathiusha22 <prathiusha22@gmail.com>
2023-02-07 18:12:03 +0530

    Applied  javascript

---
+++ assets/js/veges.js
@@ -0,0 +1,427 @@
+let vegetables = [
+
+    {
+        "product_image": {
+            "source": "../assets/image/tomato.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/cabbage.png",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Cabbage",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/beetroot.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/brocoli.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/carrot.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/potato.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/onion.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/cf.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/raddish.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/bittergourd.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/bottle.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 4
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/broadbeans.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/brinjal.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/ladysfinger.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/beans.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/chayote1.jpg",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/ridge.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/pumpkin.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/kohlrabi.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+
+    {
+        "product_image": {
+            "source": "../assets/image/garlic.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/cucumber.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/greenchilli (2).jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/ginger.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+    {
+        "product_image": {
+            "source": "../assets/image/spinach.jfif",
+            "alt": "image of tomato veggie"
+        },
+
+        "product_name": "Fresh Organic Tomato",
+
+        "price" : 150,
+
+        "rating" : 5.0
+
+    },
+
+
+
+
+
+    
+
+]
+
+
+let product_card_div;
+
+let prdouct_link;
+let product_image;
+let product_name;
+
+let product_quantity;
+
+let ratings_div;
+
+let buy_now_link;
+let add_to_cart_link;
+
+
+let i;
+
+for(i=0; i<vegetables.length; i++){
+    productCard();
+}
+
+
+function productCard(){
+
+product_card_div = document.createElement("div");
+product_card_div.setAttribute("class", "product-card");
+document.querySelector(".product-list-container").append(product_card_div);
+
+prdouct_link = document.createElement("a");
+prdouct_link.setAttribute("href", "#");
+product_card_div.append(prdouct_link);
+
+product_image = document.createElement("img");
+product_image.setAttribute("src", vegetables[i]["product_image"]["source"]);
+product_image.setAttribute("alt", vegetables[i]["product_image"]["alt"]);
+prdouct_link.append(product_image);
+
+product_name = document.createElement("p");
+product_name.innerText = vegetables[0]["product_name"];
+prdouct_link.append(product_name);
+
+
+product_quantity = document.createElement("p");
+product_quantity.innerText = "₹ "+vegetables[i]["price"]+ " Per kg";
+product_card_div.append(product_quantity);
+
+ratings_div = document.createElement("div");
+ratings_div.setAttribute("class", "product-ratings");
+product_card_div.append(ratings_div);
+
+let product = vegetables[i];
+let rating_round = Math.round(product.rating);
+
+let stars = "";
+for(let j=0; j<rating_round; j++){
+    stars += "⭐";
+}
+
+ratings_div.append(stars);
+
+buy_now_link = document.createElement("a");
+buy_now_link.setAttribute("href", "#");
+buy_now_link.setAttribute("class", "buy-now")
+buy_now_link.innerText = "Buy Now";
+product_card_div.append(buy_now_link);
+
+add_to_cart_link = document.createElement("a");
+add_to_cart_link.setAttribute("href", "#");
+add_to_cart_link.setAttribute("class" , "add-to-cart")
+add_to_cart_link.innerText = "Add to Cart";
+product_card_div.append(add_to_cart_link);
+
+}
+
