e5d486e8f068c5496770bd13b1e344434f612bcd
prathiusha22 <prathiusha22@gmail.com>
2023-05-02 15:27:28 +0530

    fix:fixed bugs in main.js page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,77 +5,88 @@ let create_product = [
         "titlename": "FreshorganicTomato",
         "price": "25",
         "ratings": "4",
-        "product_id": 0
+        "product_id": 0,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOFWga.jpg",
         "titlename": "FreshorganicBeetroot",
         "price": "30",
         "ratings": "4",
-        "product_id": 1
+        "product_id": 1,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOKxkv.jpg",
         "titlename": "FreshorganicPotato",
         "price": "40",
         "ratings": "4",
-        "product_id": 2
+        "product_id": 2,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOKAQI.jpg",
         "titlename": "FreshorganicKohlrabi",
         "price": "60",
         "ratings": "4",
-        "product_id": 3
+        "product_id": 3,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOK7EX.jpg",
         "titlename": "FreshorganicBrinjal",
         "price": "40",
         "ratings": "4",
-        "product_id": 4
+        "product_id": 4,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOK1p4.jpg",
         "titlename": "FreshorganicBittergourd",
         "price": "50",
         "ratings": "4",
-        "product_id": 5
+        "product_id": 5,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOKVQS.jpg",
         "titlename": "FreshorganicBroadbeans",
         "price": "45",
         "ratings": "4",
-        "product_id": 6
+        "product_id": 6,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOqnbR.jpg",
         "titlename": "FreshorganicBeans",
         "price": "25",
         "ratings": "4",
-        "product_id": 8
+        "product_id": 8,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOqIsI.png",
         "titlename": "FreshorganicCabbage",
         "price": "40",
         "ratings": "4",
-        "product_id": 9
+        "product_id": 9,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOqA0X.jpg",
         "titlename": "FreshorganicBrocholi",
         "price": "80",
         "ratings": "4",
-        "product_id": 10
+        "product_id": 10,
+        "status":true
     },
     {
         "image": "https://iili.io/HNOCUHg.jpg",
         "titlename": "FreshorganicCarrot",
         "price": "30",
         "ratings": "4",
-        "product_id": 11
+        "product_id": 11,
+        "status":true
     }
 ];
 
